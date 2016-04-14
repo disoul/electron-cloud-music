7cdd5a5809b382ee8d8409b7c262fbec5a98f9d1
disoul <starky_ds@sina.cn>
2016-04-15 01:54:24 +0800

    搜索api接口

--- app/components/SearchBar.jsx
+++ app/components/SearchBar.jsx
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
+import { search } from '../server';
 
 export default class SearchBar extends Component {
   constructor(props: any) {
     super(props);
   }
 
-  _onSubmit() {
-    console.log(this.refs.search.value);
+  _onSubmit(e) {
+    e.preventDefault();
+    search(this.refs.search.value, res => {
+      console.log(res);
+    });
   }
 
   render() {
     return (
       <section className="header__searchbar">
-        <form action="#" id="search-form" onSubmit={e => this._onSubmit(e)}>
+        <form id="search-form" onSubmit={e => this._onSubmit(e)}
+        >
           <label htmlFor="search">
             <img className="i" src={require("../assets/icon/search.svg")} />
           </label>
--- app/server/index.js
+++ app/server/index.js
@@ -1,5 +1,6 @@
 'use strict'
 // 因为Electron cors的问题使用本地node相互通信
+
 // id --> mp3url
 export function getSongUrl(id, callback) {
   fetch('http://localhost:11015/music/url?id=' + id.toString())
@@ -7,5 +8,17 @@ export function getSongUrl(id, callback) {
     return res.json();
   }).then( json => {
     callback(json.data[0].url);
+  } )
+}
+
+// 搜索歌曲
+export function search(keywords, callback) {
+  fetch(
+    'http://localhost:11015/search/?keywords=' + keywords + '&type=1&limit=20'
+  )
+  .then( res => {
+    return res.json();
+  }).then( json => {
+    callback(json);
   } )
 }
